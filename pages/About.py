6ed1af3636d7903cfc7aa9e179d51747427a9942
Zak <zakismail888@gmail.com>
2024-11-17 16:45:10 +0200

    normal chanhges

---
+++ pages/About.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.write("Instructions to use this Website")
+st.write("if you wanna add a todo click on the text box and type in your todo")
+
+st.write("When your done with that todo click on the checkbox to show it as 'done'")
+
+st.write("We appreciate you using our website")
+
+st.write("<b>im broke<b>",
+         unsafe_allow_html=True)
